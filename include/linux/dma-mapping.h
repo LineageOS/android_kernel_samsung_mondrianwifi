81c3ef7b8d96ed8d01f4fbaaa3d902753ef24078
Christopher R. Palmer <crpalmer@gmail.com>
2014-04-16 05:54:40 -0400

    mondrianwifi: dma: Move get_sgtable to the end of dma_ops
    Our camera and wifi drivers must be getting a copy of the DMA
    ops because adding this member into the middle of the structure
    break both of them.
    
    Moving it to the end seems to make them happy.
    
    Change-Id: Idbfed3e801fd93652b840b89227d29cf117eed51

--- include/linux/dma-mapping.h
+++ include/linux/dma-mapping.h
@@ -18,9 +18,6 @@ struct dma_map_ops {
 	int (*mmap)(struct device *, struct vm_area_struct *,
 			  void *, dma_addr_t, size_t, struct dma_attrs *attrs);
 
-	int (*get_sgtable)(struct device *dev, struct sg_table *sgt, void *,
-			   dma_addr_t, size_t, struct dma_attrs *attrs);
-
 	dma_addr_t (*map_page)(struct device *dev, struct page *page,
 			       unsigned long offset, size_t size,
 			       enum dma_data_direction dir,
@@ -54,6 +51,9 @@ struct dma_map_ops {
 	u64 (*get_required_mask)(struct device *dev);
 #endif
 	int is_phys;
+
+	int (*get_sgtable)(struct device *dev, struct sg_table *sgt, void *,
+			   dma_addr_t, size_t, struct dma_attrs *attrs);
 };
 
 #define DMA_BIT_MASK(n)	(((n) == 64) ? ~0ULL : ((1ULL<<(n))-1))
