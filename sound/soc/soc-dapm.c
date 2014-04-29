39be2b05f34eb3b9b155542c41cfd549a1289580
Aviral Gupta <aviralg@codeaurora.org>
2014-04-29 13:39:57 +0530

    ASoC: dapm: fix race condition in dapm
    Dirty widget list can be accessed by multiple paths
    in dapm framework. Dirty list is protected by dapm mutex.
    Since dapm_set_pin and dapm_force_enable_pin function
    accesses the dirty list, protect it using the dapm mutex.
    
    CRs-Fixed: 637323
    Change-Id: I8c265072083e142c0858b6cc7a983d07e7d75af0
    Signed-off-by: Aviral Gupta <aviralg@codeaurora.org>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -2160,8 +2160,10 @@ static int snd_soc_dapm_set_pin(struct snd_soc_dapm_context *dapm,
 {
 	struct snd_soc_dapm_widget *w = dapm_find_widget(dapm, pin, true);
 
+	mutex_lock_nested(&dapm->card->dapm_mutex, SND_SOC_DAPM_CLASS_PCM);
 	if (!w) {
 		dev_err(dapm->dev, "dapm: unknown pin %s\n", pin);
+		mutex_unlock(&dapm->card->dapm_mutex);
 		return -EINVAL;
 	}
 
@@ -2170,6 +2172,7 @@ static int snd_soc_dapm_set_pin(struct snd_soc_dapm_context *dapm,
 		w->force = 0;
 	dapm_mark_dirty(w, "pin configuration");
 
+	mutex_unlock(&dapm->card->dapm_mutex);
 	return 0;
 }
 
@@ -3244,8 +3247,10 @@ int snd_soc_dapm_force_enable_pin(struct snd_soc_dapm_context *dapm,
 {
 	struct snd_soc_dapm_widget *w = dapm_find_widget(dapm, pin, true);
 
+	mutex_lock_nested(&dapm->card->dapm_mutex, SND_SOC_DAPM_CLASS_PCM);
 	if (!w) {
 		dev_err(dapm->dev, "dapm: unknown pin %s\n", pin);
+		mutex_unlock(&dapm->card->dapm_mutex);
 		return -EINVAL;
 	}
 
@@ -3253,6 +3258,7 @@ int snd_soc_dapm_force_enable_pin(struct snd_soc_dapm_context *dapm,
 	w->connected = 1;
 	w->force = 1;
 	dapm_mark_dirty(w, "force enable");
+	mutex_unlock(&dapm->card->dapm_mutex);
 
 	return 0;
 }
