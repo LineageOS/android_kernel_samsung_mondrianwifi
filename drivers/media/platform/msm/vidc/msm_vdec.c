ef7c7993bd228e9afad57cc8e2af67ae3db14e81
Christopher R. Palmer <crpalmer@gmail.com>
2014-05-22 06:00:10 -0400

    mondrianwifi: vidc: Delete duplicate code
    Probably a merge issue?  The same init call was made twice.
    
    Change-Id: Icb44e90e441b8f8ff916b754d0d26497e93cf3a2

--- drivers/media/platform/msm/vidc/msm_vdec.c
+++ drivers/media/platform/msm/vidc/msm_vdec.c
@@ -980,11 +980,6 @@ int msm_vdec_s_fmt(struct msm_vidc_inst *inst, struct v4l2_format *f)
 			rc = -EINVAL;
 			goto err_invalid_fmt;
 		}
-		rc = msm_comm_try_state(inst, MSM_VIDC_CORE_INIT_DONE);
-		if (rc) {
-			dprintk(VIDC_ERR, "Failed to initialize instance\n");
-			goto err_invalid_fmt;
-		}
 
 		rc = msm_comm_try_state(inst, MSM_VIDC_CORE_INIT_DONE);
 		if (rc) {
