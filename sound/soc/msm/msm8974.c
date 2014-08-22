efce5e0f0d90ca39f97777e9c57606eadb35b192
Christopher R. Palmer <crpalmer@gmail.com>
2014-08-22 16:21:29 -0400

    sound: Tone down Samsung log spam
    Change-Id: I999185832a94566dbc42ff4892116efe5b2b1092

--- sound/soc/msm/msm8974.c
+++ sound/soc/msm/msm8974.c
@@ -605,7 +605,7 @@ static int msm_ext_spkramp_event(struct snd_soc_dapm_widget *w,
 static int msm_ext_spkramp_event(struct snd_soc_dapm_widget *w,
 			     struct snd_kcontrol *k, int event)
 {
-	pr_info("%s() : control =%d\n", __func__, event);
+	pr_debug("%s() : control =%d\n", __func__, event);
 
 	gpio_direction_output(spkamp_en_gpio,
 			SND_SOC_DAPM_EVENT_ON(event));
@@ -705,7 +705,7 @@ static int msm8974_mclk_event(struct snd_soc_dapm_widget *w,
 static int msm_mainmic_bias_event(struct snd_soc_dapm_widget *w,
 	struct snd_kcontrol *k, int event)
 {
-	pr_info("%s : Event %d,  SND_SOC_DAPM:%d\n",
+	pr_debug("%s : Event %d,  SND_SOC_DAPM:%d\n",
 		__func__, (event), SND_SOC_DAPM_EVENT_ON(event));
 
 	if (mainmic_bias_gpio < 0) {
@@ -720,7 +720,7 @@ static int msm_mainmic_bias_event(struct snd_soc_dapm_widget *w,
 		if(main_mic_delay != 100)
 			main_mic_delay *= 50;
 		msleep(main_mic_delay);
-		pr_info("%s: main_mic_delay = %d\n", __func__, main_mic_delay);
+		pr_debug("%s: main_mic_delay = %d\n", __func__, main_mic_delay);
 		main_mic_delay = 0;
     }
 
@@ -818,7 +818,7 @@ static int slim0_rx_sample_rate_put(struct snd_kcontrol *kcontrol,
 		slim0_rx_sample_rate = SAMPLING_RATE_48KHZ;
 	}
 
-	pr_info("%s: slim0_rx_sample_rate = %d\n", __func__,
+	pr_debug("%s: slim0_rx_sample_rate = %d\n", __func__,
 			slim0_rx_sample_rate);
 
 	return 0;
@@ -1083,7 +1083,7 @@ static int msm8974_auxpcm_rate_put(struct snd_kcontrol *kcontrol,
 		msm_btsco_rate = BTSCO_RATE_8KHZ;
 		break;
 	}
-	pr_info("%s: BT sample rate = %d ====\n",__func__, msm8974_auxpcm_rate);
+	pr_debug("%s: BT sample rate = %d ====\n",__func__, msm8974_auxpcm_rate);
 	return 0;
 }
 
@@ -1192,7 +1192,7 @@ static int msm8974_hdmi_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	struct snd_interval *channels = hw_param_interval(params,
 					SNDRV_PCM_HW_PARAM_CHANNELS);
 
-	pr_info("%s channels->min %u channels->max %u ()\n", __func__,
+	pr_debug("%s channels->min %u channels->max %u ()\n", __func__,
 			channels->min, channels->max);
 
 	param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
@@ -1388,7 +1388,7 @@ static int msm_slim_0_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	rate->min = rate->max = slim0_rx_sample_rate;
 	channels->min = channels->max = msm_slim_0_rx_ch;
 
-	 pr_info("%s: format = %d, rate = %d, channels = %d\n",
+	 pr_debug("%s: format = %d, rate = %d, channels = %d\n",
 			  __func__, params_format(params), params_rate(params),
 			  msm_slim_0_rx_ch);
 
@@ -1410,7 +1410,7 @@ static int msm_slim_0_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	rate->min = rate->max = 48000;
 	channels->min = channels->max = msm_slim_0_tx_ch;
 
-	 pr_info("%s: format = %d, rate = %d, channels = %d\n",
+	 pr_debug("%s: format = %d, rate = %d, channels = %d\n",
 			  __func__, params_format(params), params_rate(params),
 			  msm_slim_0_tx_ch);
 
@@ -1880,7 +1880,7 @@ static int msm_snd_hw_params(struct snd_pcm_substream *substream,
 			user_set_tx_ch = params_channels(params);
 		else
 			user_set_tx_ch = tx_ch_cnt;
-		pr_info("%s: msm_slim_0_tx_ch(%d)user_set_tx_ch(%d)tx_ch_cnt(%d)\n",
+		pr_debug("%s: msm_slim_0_tx_ch(%d)user_set_tx_ch(%d)tx_ch_cnt(%d)\n",
 			 __func__, msm_slim_0_tx_ch, user_set_tx_ch, tx_ch_cnt);
 
 		ret = snd_soc_dai_set_channel_map(cpu_dai,
