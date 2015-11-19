1e55bb22a00839c2a5e4ab0f9b306074393f7b90
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-19 06:46:40 -0500

    wcnss: Fix caf's improper ifdef'ing
    Change-Id: I809fb14e295a5e446143e41fb433d5ff1cf127d9

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -946,8 +946,10 @@ void wcnss_reset_intr(void)
 {
 	if (wcnss_hardware_type() == WCNSS_PRONTO_HW) {
 		wcnss_pronto_log_debug_regs();
+#ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
 		if (wcnss_get_mux_control())
 			wcnss_log_iris_regs();
+#endif
 		wmb();
 		__raw_writel(1 << 16, penv->fiq_reg);
 	} else {
@@ -965,8 +967,10 @@ void wcnss_reset_fiq(bool clk_chk_en)
 			wcnss_log_debug_regs_on_bite();
 		} else {
 			wcnss_pronto_log_debug_regs();
+#ifdef CONFIG_WCNSS_REGISTER_DUMP_ON_BITE
 			if (wcnss_get_mux_control())
 				wcnss_log_iris_regs();
+#endif
 		}
 		/* Insert memory barrier before writing fiq register */
 		wmb();
